e3ee0c4b7466c32fa1a2c1a0c802f1402dcdbd5e
AWGConnorSmith <143447517+AWGConnorSmith@users.noreply.github.com>
2024-06-26 09:58:47 -0500

    Update Main.java

--- FeatureTest/src/com/ft/Main.java
+++ FeatureTest/src/com/ft/Main.java
@@ -1,9 +1,12 @@
 package com.ft;
 
+import com.ft.Sample;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Sample s = new Sample("Apple", "Semi round fruit with a tough peel. Usually characterized as red but can be a wide variety of colors.");
+		System.out.println(s.getDescription());
 
 	}
 
