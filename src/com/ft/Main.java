d971adecafcad5fa9da9af4a64c10bd5c5cecaf4
AWGConnorSmith <143447517+AWGConnorSmith@users.noreply.github.com>
2024-07-03 15:39:28 -0500

    Set up a base java project with codeQl yml file for scanning

---
+++ src/com/ft/Main.java
@@ -0,0 +1,10 @@
+package com.ft;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Sample s = new Sample("Apple", "Semi round fruit with a tough peel. Usually characterized as red but can be a wide variety of colors.");
+		System.out.println(s.toJson());
+	}
+
+}
